1d2ef6d596a1b03f37885aa53069fbc67c78aee5
Moni Lamas <37511909+moniLamas@users.noreply.github.com>
2021-03-18 14:26:04 +0100

    change footer__text footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -37,12 +37,12 @@ const Footer = () => {
             })}
         </div>
         <hr />
-        /*<p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
+        <p className="footer__text">
+          © {new Date().getFullYear()} - Template developed by{'Moni Lamas'}
           <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
             Jacobo Martínez
           </a>
-        </p>*/
+        </p>
 
         {isEnabled && <GithubButtons />}
       </Container>
